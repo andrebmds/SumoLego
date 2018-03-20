587ab9c5141ba2389eaab931afd400b4346e55e1
Andre Bortoli <andrebmds@live.com>
2018-03-19 21:44:05 -0300

    Start structure

---
+++ Python/RobotComunication.py
@@ -0,0 +1,31 @@
+import nxt.locator
+from nxt.motor import *
+from nxt.sensor import *
+
+
+class ntx_robot():
+    # INPUT
+    ultrasonic = PORT_1
+    light = PORT_2
+    color = PORT_3
+    gyroscope = PORT_4
+    # OUTPUT
+    leftMotor = PORT_A
+    rightMotor = PORT_B
+
+    def __int__(self):
+        pass
+
+    def input_sensor(self):
+        # Ultrasonic
+        # Light sensor
+        # Color sensor
+        # Gyroscope
+        # Port used for sensor
+        pass
+
+    def output_motor(self, left, right):
+        # Right Motor
+        # Left Motor
+        pass
+# class motor():
